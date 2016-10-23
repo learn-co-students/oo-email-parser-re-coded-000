cc61d98f02a443b9565ae14325da19f6b6a7be34
Yardagan AL Hamed <gooryalhamed@hotmail.com>
2016-10-23 17:54:41 +0000

    shorter soul

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,36 +3,15 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailParser
-  attr_accessor :emails
+  attr_reader :emails
+
   def initialize(emails)
     @emails = emails
   end
+
   def parse
-    stripped_arr =[]
-    arr1 =[]
-    arr2= []
-    arr3 =[]
-    reject_arr=[]
-    final_arr =[]
-    parser = EmailParser.new(emails)
-      #"avi@test.com, arel@test.com test@avi.com, test@arel.com"
-      arr1 = emails.split(",")
-      stripped_arr = arr1.each do |item|
-        stripped_arr << item.strip
-      end
-      reject_arr = stripped_arr.reject do |e|
-        e.include?(" ")
-      end
-      reject_arr.each do |em|
-        final_arr << em
-      end
-    arr2 = stripped_arr.select do |item|
-        item.include?(" ")
-      end
-      arr3 = arr2.join.split(" ")
-      arr3.each do |e|
-      final_arr  << e
-      end
-      final_arr.uniq
+    emails.split.map do |email|
+      email.split(',')
+    end.flatten.uniq
   end
 end
